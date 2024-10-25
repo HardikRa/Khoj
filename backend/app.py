264ed8c146872619dc709f5ccbbd361f3cc39af5
jaden <furtadojaden@gmail.com>
2024-10-26 00:04:30 +0530

    Add banckend neo4j queries

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask,request
+from common.neo4j_utils import Neo4jUtils
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/fradulent_users')
 def index(methods=['GET','POST']):
+    query_type = request.args.get("filter")    
+    neo4jUtils = Neo4jUtils('neo4j','p2ppassword','neo4j://10.1.210.104')
+    result = neo4jUtils.filter_fraudlent_user()
+    for res in result:
+        print(res)
     return 'Web App with Python Flask!'
 
 @app.route('/llm')
--- backend/common/neo4j_utils.py
+++ backend/common/neo4j_utils.py
@@ -1,23 +1,29 @@
 import neo4j
 
-class neo4jUtils:
+class Neo4jUtils:
     def __init__(self,username:str,password:str,uri:str):
         self.username = username
         self.password = password
         self.uri = uri
         self.driver = neo4j.GraphDatabase.driver(self.uri, auth=(self.username, self.password))
 
-    def execute_query(self,query:str):
+    def execute_query(self):
         query = """
-        MERGE (d:Domain {name: $domain})
-        WITH d
-        UNWIND $subdomains AS subdomain
-        MERGE (s:Domain {name: subdomain})
-        MERGE (d)-[:HAS_SUBDOMAIN]->(s)
-        RETURN d, s
+        MATCH (n) RETURN n LIMIT 100;
         """
-        with self.driver.session() as session:
-            # result = session.run(query, domain=domain, subdomains=subdomains)
-            # return [(record["d"]["name"], record["s"]["name"]) for record in result]
-            pass
+        with self.driver.session(database="db2") as session:
+            result = session.run(query)
+            return [record for record in result]
 
+        
+    def filter_fraudlent_user(self):
+        """
+        Filter fraudlent users.
+        Return a list of records
+        """
+        query = """
+                MATCH (u:User) where u.fraudMoneyTransfer=1 return u LIMIT 100;
+        """
+        with self.driver.session(database="db2") as session:
+            result = session.run(query)
+            return [record for record in result]
