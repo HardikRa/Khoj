7068e2f50763c3bdf8f5f8229aebf3b8bfefb821
jaden <furtadojaden@gmail.com>
2024-10-25 19:47:26 +0530

    Save gitignore

--- backend/app.py
+++ backend/app.py
@@ -3,8 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def index(methods=['GET','POST']):
     return 'Web App with Python Flask!'
+
+@app.route('/llm')
+def llm():
+    pass
 
 if __name__=='__main__':
     app.run(host='0.0.0.0', port=8000)
